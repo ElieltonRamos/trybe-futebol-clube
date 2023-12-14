60605f798bad545f9b700fd5f22387310ef04917
Elielton <elieltonramos14@gmail.com>
2023-12-13 23:43:14 -0300

    Refatorando rota login

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -8,16 +8,24 @@ class LoginService {
     private model = new UsersModel(),
   ) { }
 
+  async validateUser(password: string, email: string) {
+    const user = await this.model.findByEmail(email);
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!user || !regexEmail.test(email)) return false;
+    const passwordValid = password.length < 6 || !compareSync(password, user.password);
+    if (passwordValid) return false;
+    return user;
+  }
+
   async login(email: string, password: string): Promise<ServiceResponse<{ token: string }>> {
     if (!email || !password) {
       return { status: 'badRequest', data: { message: 'All fields must be filled' } };
     }
-    const user = await this.model.findByEmail(email);
-    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if (user === null || !compareSync(password, user.password) || !regexEmail.test(email) || password.length < 6) {
+    const validUser = await this.validateUser(password, email);
+    if (!validUser) {
       return { status: 'unauthorized', data: { message: 'Invalid email or password' } };
     }
-    const token = JsonWebToken.generateToken({ id: user.id, userName: user.username });
+    const token = JsonWebToken.generateToken({ id: validUser.id, userName: validUser.username });
     return { status: 'ok', data: { token } };
   }
 }
