2b1477dc282f3f2b93d697ca845f9f193de15c32
Elielton <elieltonramos14@gmail.com>
2024-01-07 12:29:54 -0300

    Criando logica para reunir e tratar dados da rota '/leaderboard/home'

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -1,8 +1,27 @@
+// import IMatches from '../Interfaces/IMatches';
 import { ServiceResponse } from '../Interfaces/IServicesResponse';
+import LeadboardModel from '../models/leadboardModel';
+
+export type matchsTeamsHome = {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+};
 
 class leaderboardService {
-  async leaderboardSearchHome(): Promise<ServiceResponse<string>> {
-    return { status: 'ok', data: 'ovofaze' };
+  constructor(
+    private leaderbordModel = new LeadboardModel(),
+  ) { }
+
+  async leaderboardSearchHome(): Promise<ServiceResponse<matchsTeamsHome[]>> {
+    const matchsTeamsHome = await this.leaderbordModel.searchLeadboardHome();
+
+    return { status: 'ok', data: matchsTeamsHome };
   }
 }
 
